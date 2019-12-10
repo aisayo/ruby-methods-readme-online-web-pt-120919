0b2a7044f8340f2db9d1cdf81bd1e5498da86fea
Aysan Isayo <aisayo@icloud.com>
2019-12-10 17:24:52 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -4,5 +4,15 @@
 
 def say_hello_world_ten_times
   puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+  puts "Hello World"
+
   
 end 
